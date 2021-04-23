73c4273182dbf6f58f9098c5d80e06b7068d5dd5
j01 <jevalen4868@yahoo.com>
2021-04-22 18:24:52 -0700

    feature: returning articles

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,11 +12,29 @@ func homePage(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Endpoint hit! Homepage")
 }
 
+func returnAllArticles(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Endpoint hit! Articles")
+	json.NewEncoder(writer).Encode(Articles)
+}
+
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Description", Content:"Content"},
+		Article{Title: "Hello1", Desc: "Description1", Content:"Content1"},
+	}
 	handleRequests()
 }
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:Content`
+}
+
+var Articles []Article
