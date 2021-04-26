a0fffa986addb19e0ebe0c507c25a2a63644d250
j01 <jevalen4868@yahoo.com>
2021-04-25 18:37:54 -0700

    feature: added gorilla mux

--- main.go
+++ main.go
@@ -5,33 +5,52 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
-func homePage(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Welcome to the homepage!")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the homepage!")
 	fmt.Println("Endpoint hit! Homepage")
 }
 
-func returnAllArticles(writer http.ResponseWriter, request *http.Request) {
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint hit! Articles")
-	json.NewEncoder(writer).Encode(Articles)
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	fmt.Fprintf(w, "Key: " + key)
+	for _, a := range Articles {
+		if a.Id == key {
+			json.NewEncoder(w).Encode(a)
+			return
+		}
+	}
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", returnAllArticles)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/", homePage)
+	r.HandleFunc("/articles", returnAllArticles)
+	r.HandleFunc("/articles/{id}", returnArticle)
+	//http.HandleFunc("/", homePage)
+	//http.HandleFunc("/articles", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":10000", r))
 }
 
 func main() {
+	fmt.Println("Rest API v2. - Mux Routers")
 	Articles = []Article{
-		Article{Title: "Hello", Desc: "Description", Content:"Content"},
-		Article{Title: "Hello1", Desc: "Description1", Content:"Content1"},
+		Article{Id: "1", Title: "Hello", Desc: "Description", Content:"Content"},
+		Article{Id: "2", Title: "Hello1", Desc: "Description1", Content:"Content1"},
 	}
 	handleRequests()
 }
 
 type Article struct {
+	Id string `json:"Id"`
 	Title string `json:"Title"`
 	Desc string `json:"desc"`
 	Content string `json:Content`
